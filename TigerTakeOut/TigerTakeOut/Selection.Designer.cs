f200d8d2c9d45659aacd212d3a0e19f588c56563
julshotal <jgh6364@IGM2435-32>
2018-12-03 12:32:59 -0500

    formatting of selection page plus individual resturant menu pages

--- TigerTakeOut/TigerTakeOut/Selection.Designer.cs
+++ TigerTakeOut/TigerTakeOut/Selection.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(454, 345);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(665, 44);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Where would you like to order from?";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Mandarin Court Chinese Resturant",
+            "Applebee\'s",
+            "Blaze Pizza",
+            "Moe\'s Southwest Grill",
+            "Texas Bar-B-Q Joint"});
+            this.comboBox1.Location = new System.Drawing.Point(462, 417);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(657, 27);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Resturant";
+            // 
+            // Selection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::TigerTakeOut.Properties.Resources.tigertakeoutbg1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Selection";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
