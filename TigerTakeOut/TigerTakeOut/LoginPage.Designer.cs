f1ec8b2aed4806108dc48aa7212f16cd0364d1b8
Henry Grant <hrg5041@rit.edu>
2018-12-03 12:50:42 -0500

    loginpage added

---
+++ TigerTakeOut/TigerTakeOut/LoginPage.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TigerTakeOut
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginPage));
+            this.loginBackground = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.loginBackground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loginBackground
+            // 
+            this.loginBackground.Image = ((System.Drawing.Image)(resources.GetObject("loginBackground.Image")));
+            this.loginBackground.Location = new System.Drawing.Point(0, 0);
+            this.loginBackground.Name = "loginBackground";
+            this.loginBackground.Size = new System.Drawing.Size(1600, 900);
+            this.loginBackground.TabIndex = 0;
+            this.loginBackground.TabStop = false;
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.loginBackground);
+            this.Name = "LoginPage";
+            this.Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)(this.loginBackground)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox loginBackground;
+    }
+}
