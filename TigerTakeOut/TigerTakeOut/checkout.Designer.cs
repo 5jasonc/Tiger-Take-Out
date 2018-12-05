654687ed2be48f28c749b4012c28c26609d9f8dd
Jin Jin Heipler <heipler.jj@gmail.com>
2018-12-05 12:52:40 -0500

    added items and prices

--- TigerTakeOut/TigerTakeOut/checkout.Designer.cs
+++ TigerTakeOut/TigerTakeOut/checkout.Designer.cs
@@ -30,6 +30,24 @@
         {
             this.shoppingCartLabel = new System.Windows.Forms.Label();
             this.graybackground = new System.Windows.Forms.PictureBox();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.cartItemsLabel = new System.Windows.Forms.Label();
+            this.cartTotalLabel = new System.Windows.Forms.Label();
+            this.item1 = new System.Windows.Forms.Label();
+            this.item2 = new System.Windows.Forms.Label();
+            this.item3 = new System.Windows.Forms.Label();
+            this.item5 = new System.Windows.Forms.Label();
+            this.item4 = new System.Windows.Forms.Label();
+            this.price1 = new System.Windows.Forms.Label();
+            this.price2 = new System.Windows.Forms.Label();
+            this.price3 = new System.Windows.Forms.Label();
+            this.price4 = new System.Windows.Forms.Label();
+            this.price5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.subtotal = new System.Windows.Forms.Label();
+            this.taxes = new System.Windows.Forms.Label();
+            this.taxesLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.graybackground)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,6 +72,208 @@
             this.graybackground.TabIndex = 1;
             this.graybackground.TabStop = false;
             // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.BackColor = System.Drawing.Color.Transparent;
+            this.userLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userLabel.ForeColor = System.Drawing.Color.White;
+            this.userLabel.Location = new System.Drawing.Point(1368, 143);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(62, 18);
+            this.userLabel.TabIndex = 2;
+            this.userLabel.Text = "jxh9922";
+            // 
+            // cartItemsLabel
+            // 
+            this.cartItemsLabel.AutoSize = true;
+            this.cartItemsLabel.BackColor = System.Drawing.Color.Transparent;
+            this.cartItemsLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cartItemsLabel.ForeColor = System.Drawing.Color.White;
+            this.cartItemsLabel.Location = new System.Drawing.Point(1436, 143);
+            this.cartItemsLabel.Name = "cartItemsLabel";
+            this.cartItemsLabel.Size = new System.Drawing.Size(110, 18);
+            this.cartItemsLabel.TabIndex = 3;
+            this.cartItemsLabel.Text = "|   Items in cart:";
+            // 
+            // cartTotalLabel
+            // 
+            this.cartTotalLabel.AutoSize = true;
+            this.cartTotalLabel.BackColor = System.Drawing.Color.Transparent;
+            this.cartTotalLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cartTotalLabel.ForeColor = System.Drawing.Color.White;
+            this.cartTotalLabel.Location = new System.Drawing.Point(1542, 143);
+            this.cartTotalLabel.Name = "cartTotalLabel";
+            this.cartTotalLabel.Size = new System.Drawing.Size(17, 18);
+            this.cartTotalLabel.TabIndex = 4;
+            this.cartTotalLabel.Text = "0";
+            // 
+            // item1
+            // 
+            this.item1.AutoSize = true;
+            this.item1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(214)))));
+            this.item1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.item1.Location = new System.Drawing.Point(295, 322);
+            this.item1.Name = "item1";
+            this.item1.Size = new System.Drawing.Size(246, 27);
+            this.item1.TabIndex = 5;
+            this.item1.Text = "Chicken Noodle Soup";
+            // 
+            // item2
+            // 
+            this.item2.AutoSize = true;
+            this.item2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(214)))));
+            this.item2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.item2.Location = new System.Drawing.Point(295, 366);
+            this.item2.Name = "item2";
+            this.item2.Size = new System.Drawing.Size(246, 27);
+            this.item2.TabIndex = 6;
+            this.item2.Text = "Chicken Noodle Soup";
+            // 
+            // item3
+            // 
+            this.item3.AutoSize = true;
+            this.item3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(214)))));
+            this.item3.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.item3.Location = new System.Drawing.Point(295, 410);
+            this.item3.Name = "item3";
+            this.item3.Size = new System.Drawing.Size(246, 27);
+            this.item3.TabIndex = 7;
+            this.item3.Text = "Chicken Noodle Soup";
+            // 
+            // item5
+            // 
+            this.item5.AutoSize = true;
+            this.item5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(214)))));
+            this.item5.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.item5.Location = new System.Drawing.Point(295, 505);
+            this.item5.Name = "item5";
+            this.item5.Size = new System.Drawing.Size(246, 27);
+            this.item5.TabIndex = 9;
+            this.item5.Text = "Chicken Noodle Soup";
+            // 
+            // item4
+            // 
+            this.item4.AutoSize = true;
+            this.item4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(214)))));
+            this.item4.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.item4.Location = new System.Drawing.Point(295, 458);
+            this.item4.Name = "item4";
+            this.item4.Size = new System.Drawing.Size(246, 27);
+            this.item4.TabIndex = 8;
+            this.item4.Text = "Chicken Noodle Soup";
+            // 
+            // price1
+            // 
+            this.price1.AutoSize = true;
+            this.price1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(214)))));
+            this.price1.Font = new System.Drawing.Font("Arial", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.price1.Location = new System.Drawing.Point(701, 323);
+            this.price1.Name = "price1";
+            this.price1.Size = new System.Drawing.Size(70, 26);
+            this.price1.TabIndex = 10;
+            this.price1.Text = "$5.00";
+            // 
+            // price2
+            // 
+            this.price2.AutoSize = true;
+            this.price2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(214)))));
+            this.price2.Font = new System.Drawing.Font("Arial", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.price2.Location = new System.Drawing.Point(701, 367);
+            this.price2.Name = "price2";
+            this.price2.Size = new System.Drawing.Size(70, 26);
+            this.price2.TabIndex = 11;
+            this.price2.Text = "$5.00";
+            // 
+            // price3
+            // 
+            this.price3.AutoSize = true;
+            this.price3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(214)))));
+            this.price3.Font = new System.Drawing.Font("Arial", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.price3.Location = new System.Drawing.Point(701, 411);
+            this.price3.Name = "price3";
+            this.price3.Size = new System.Drawing.Size(70, 26);
+            this.price3.TabIndex = 12;
+            this.price3.Text = "$5.00";
+            // 
+            // price4
+            // 
+            this.price4.AutoSize = true;
+            this.price4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(214)))));
+            this.price4.Font = new System.Drawing.Font("Arial", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.price4.Location = new System.Drawing.Point(701, 459);
+            this.price4.Name = "price4";
+            this.price4.Size = new System.Drawing.Size(70, 26);
+            this.price4.TabIndex = 13;
+            this.price4.Text = "$5.00";
+            // 
+            // price5
+            // 
+            this.price5.AutoSize = true;
+            this.price5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(214)))));
+            this.price5.Font = new System.Drawing.Font("Arial", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.price5.Location = new System.Drawing.Point(701, 506);
+            this.price5.Name = "price5";
+            this.price5.Size = new System.Drawing.Size(70, 26);
+            this.price5.TabIndex = 14;
+            this.price5.Text = "$5.00";
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(300, 558);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(471, 2);
+            this.label1.TabIndex = 15;
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.AutoSize = true;
+            this.subtotalLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(214)))));
+            this.subtotalLabel.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotalLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.subtotalLabel.Location = new System.Drawing.Point(295, 590);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(86, 24);
+            this.subtotalLabel.TabIndex = 16;
+            this.subtotalLabel.Text = "Subtotal";
+            // 
+            // subtotal
+            // 
+            this.subtotal.AutoSize = true;
+            this.subtotal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(214)))));
+            this.subtotal.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotal.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.subtotal.Location = new System.Drawing.Point(702, 590);
+            this.subtotal.Name = "subtotal";
+            this.subtotal.Size = new System.Drawing.Size(76, 24);
+            this.subtotal.TabIndex = 17;
+            this.subtotal.Text = "$25.00";
+            // 
+            // taxes
+            // 
+            this.taxes.AutoSize = true;
+            this.taxes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(214)))));
+            this.taxes.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxes.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.taxes.Location = new System.Drawing.Point(703, 637);
+            this.taxes.Name = "taxes";
+            this.taxes.Size = new System.Drawing.Size(76, 24);
+            this.taxes.TabIndex = 19;
+            this.taxes.Text = "$25.00";
+            // 
+            // taxesLabel
+            // 
+            this.taxesLabel.AutoSize = true;
+            this.taxesLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(214)))));
+            this.taxesLabel.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxesLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.taxesLabel.Location = new System.Drawing.Point(296, 637);
+            this.taxesLabel.Name = "taxesLabel";
+            this.taxesLabel.Size = new System.Drawing.Size(65, 24);
+            this.taxesLabel.TabIndex = 18;
+            this.taxesLabel.Text = "Taxes";
+            // 
             // checkout
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -61,6 +281,24 @@
             this.BackgroundImage = global::TigerTakeOut.Properties.Resources.tigertakeoutbg1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.taxes);
+            this.Controls.Add(this.taxesLabel);
+            this.Controls.Add(this.subtotal);
+            this.Controls.Add(this.subtotalLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.price5);
+            this.Controls.Add(this.price4);
+            this.Controls.Add(this.price3);
+            this.Controls.Add(this.price2);
+            this.Controls.Add(this.price1);
+            this.Controls.Add(this.item5);
+            this.Controls.Add(this.item4);
+            this.Controls.Add(this.item3);
+            this.Controls.Add(this.item2);
+            this.Controls.Add(this.item1);
+            this.Controls.Add(this.cartTotalLabel);
+            this.Controls.Add(this.cartItemsLabel);
+            this.Controls.Add(this.userLabel);
             this.Controls.Add(this.graybackground);
             this.Controls.Add(this.shoppingCartLabel);
             this.Name = "checkout";
@@ -76,6 +314,24 @@
 
         private System.Windows.Forms.Label shoppingCartLabel;
         private System.Windows.Forms.PictureBox graybackground;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Label cartItemsLabel;
+        private System.Windows.Forms.Label cartTotalLabel;
+        private System.Windows.Forms.Label item1;
+        private System.Windows.Forms.Label item2;
+        private System.Windows.Forms.Label item3;
+        private System.Windows.Forms.Label item5;
+        private System.Windows.Forms.Label item4;
+        private System.Windows.Forms.Label price1;
+        private System.Windows.Forms.Label price2;
+        private System.Windows.Forms.Label price3;
+        private System.Windows.Forms.Label price4;
+        private System.Windows.Forms.Label price5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.Label subtotal;
+        private System.Windows.Forms.Label taxes;
+        private System.Windows.Forms.Label taxesLabel;
     }
 }
 
