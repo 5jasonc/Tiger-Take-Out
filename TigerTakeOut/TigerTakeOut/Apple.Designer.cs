7f955c2bd8f6659539086c61f28c7cfee47feedb
Jin Jin Heipler <heipler.jj@gmail.com>
2018-12-07 12:47:54 -0500

    named all the buttons

--- TigerTakeOut/TigerTakeOut/Apple.Designer.cs
+++ TigerTakeOut/TigerTakeOut/Apple.Designer.cs
@@ -28,66 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
-            this.button12 = new System.Windows.Forms.Button();
+            this.app4 = new System.Windows.Forms.Button();
+            this.app3 = new System.Windows.Forms.Button();
+            this.sand2 = new System.Windows.Forms.Button();
+            this.sand1 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
+            this.app2 = new System.Windows.Forms.Button();
+            this.app1 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.chick1 = new System.Windows.Forms.Button();
+            this.chick2 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.grill4 = new System.Windows.Forms.Button();
+            this.grill3 = new System.Windows.Forms.Button();
+            this.grill2 = new System.Windows.Forms.Button();
+            this.grill1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.button13 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button9
+            // app4
             // 
-            this.button9.Location = new System.Drawing.Point(968, 721);
-            this.button9.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(233, 84);
-            this.button9.TabIndex = 51;
-            this.button9.Text = "Mozzarella Sticks\r\n$8.29";
-            this.button9.UseVisualStyleBackColor = true;
+            this.app4.Location = new System.Drawing.Point(968, 721);
+            this.app4.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.app4.Name = "app4";
+            this.app4.Size = new System.Drawing.Size(233, 84);
+            this.app4.TabIndex = 51;
+            this.app4.Text = "Mozzarella Sticks\r\n8.29";
+            this.app4.UseVisualStyleBackColor = true;
             // 
-            // button10
+            // app3
             // 
-            this.button10.Location = new System.Drawing.Point(720, 721);
-            this.button10.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(233, 84);
-            this.button10.TabIndex = 50;
-            this.button10.Text = "Boneless Wings\r\n$9.99";
-            this.button10.UseVisualStyleBackColor = true;
+            this.app3.Location = new System.Drawing.Point(720, 721);
+            this.app3.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.app3.Name = "app3";
+            this.app3.Size = new System.Drawing.Size(233, 84);
+            this.app3.TabIndex = 50;
+            this.app3.Text = "Boneless Wings\r\n9.99";
+            this.app3.UseVisualStyleBackColor = true;
             // 
-            // button11
+            // sand2
             // 
-            this.button11.Location = new System.Drawing.Point(446, 721);
-            this.button11.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(233, 84);
-            this.button11.TabIndex = 49;
-            this.button11.Text = "Zesty Chicken Sandwich\r\n$9.99";
-            this.button11.UseVisualStyleBackColor = true;
+            this.sand2.Location = new System.Drawing.Point(446, 721);
+            this.sand2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.sand2.Name = "sand2";
+            this.sand2.Size = new System.Drawing.Size(233, 84);
+            this.sand2.TabIndex = 49;
+            this.sand2.Text = "Zesty Chicken Sandwich\r\n9.99";
+            this.sand2.UseVisualStyleBackColor = true;
             // 
-            // button12
+            // sand1
             // 
-            this.button12.Location = new System.Drawing.Point(193, 721);
-            this.button12.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.button12.Name = "button12";
-            this.button12.Size = new System.Drawing.Size(233, 84);
-            this.button12.TabIndex = 48;
-            this.button12.Text = "Clubhouse Grille\r\n$10.99";
-            this.button12.UseVisualStyleBackColor = true;
+            this.sand1.Location = new System.Drawing.Point(193, 721);
+            this.sand1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.sand1.Name = "sand1";
+            this.sand1.Size = new System.Drawing.Size(233, 84);
+            this.sand1.TabIndex = 48;
+            this.sand1.Text = "Clubhouse Grille\r\n10.99";
+            this.sand1.UseVisualStyleBackColor = true;
             // 
             // label5
             // 
@@ -100,25 +100,25 @@
             this.label5.TabIndex = 47;
             this.label5.Text = "Sandwiches";
             // 
-            // button8
+            // app2
             // 
-            this.button8.Location = new System.Drawing.Point(968, 545);
-            this.button8.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(233, 84);
-            this.button8.TabIndex = 46;
-            this.button8.Text = "Neighborhood Nachos Beef\r\n$10.59";
-            this.button8.UseVisualStyleBackColor = true;
+            this.app2.Location = new System.Drawing.Point(968, 545);
+            this.app2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.app2.Name = "app2";
+            this.app2.Size = new System.Drawing.Size(233, 84);
+            this.app2.TabIndex = 46;
+            this.app2.Text = "Neighborhood Nachos Beef\r\n10.59";
+            this.app2.UseVisualStyleBackColor = true;
             // 
-            // button7
+            // app1
             // 
-            this.button7.Location = new System.Drawing.Point(720, 545);
-            this.button7.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(233, 84);
-            this.button7.TabIndex = 45;
-            this.button7.Text = "Breadsticks with Alfredo Sauce\r\n$5.49";
-            this.button7.UseVisualStyleBackColor = true;
+            this.app1.Location = new System.Drawing.Point(720, 545);
+            this.app1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.app1.Name = "app1";
+            this.app1.Size = new System.Drawing.Size(233, 84);
+            this.app1.TabIndex = 45;
+            this.app1.Text = "Breadsticks with Alfredo Sauce\r\n5.49";
+            this.app1.UseVisualStyleBackColor = true;
             // 
             // label4
             // 
@@ -131,25 +131,25 @@
             this.label4.TabIndex = 44;
             this.label4.Text = "Appetizers";
             // 
-            // button6
+            // chick1
             // 
-            this.button6.Location = new System.Drawing.Point(193, 545);
-            this.button6.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(233, 84);
-            this.button6.TabIndex = 43;
-            this.button6.Text = "Chicken Tenders Plate\r\n$10.69";
-            this.button6.UseVisualStyleBackColor = true;
+            this.chick1.Location = new System.Drawing.Point(193, 545);
+            this.chick1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.chick1.Name = "chick1";
+            this.chick1.Size = new System.Drawing.Size(233, 84);
+            this.chick1.TabIndex = 43;
+            this.chick1.Text = "Chicken Tenders Plate\r\n10.69";
+            this.chick1.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // chick2
             // 
-            this.button5.Location = new System.Drawing.Point(446, 545);
-            this.button5.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(233, 84);
-            this.button5.TabIndex = 42;
-            this.button5.Text = "Fiesta Lime Chicken\r\n$12.49";
-            this.button5.UseVisualStyleBackColor = true;
+            this.chick2.Location = new System.Drawing.Point(446, 545);
+            this.chick2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.chick2.Name = "chick2";
+            this.chick2.Size = new System.Drawing.Size(233, 84);
+            this.chick2.TabIndex = 42;
+            this.chick2.Text = "Fiesta Lime Chicken\r\n12.49";
+            this.chick2.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
@@ -162,45 +162,45 @@
             this.label3.TabIndex = 41;
             this.label3.Text = "Chicken";
             // 
-            // button4
+            // grill4
             // 
-            this.button4.Location = new System.Drawing.Point(968, 360);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(233, 84);
-            this.button4.TabIndex = 40;
-            this.button4.Text = "8 oz. Top Sirloin\r\n$15.49";
-            this.button4.UseVisualStyleBackColor = true;
+            this.grill4.Location = new System.Drawing.Point(968, 360);
+            this.grill4.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.grill4.Name = "grill4";
+            this.grill4.Size = new System.Drawing.Size(233, 84);
+            this.grill4.TabIndex = 40;
+            this.grill4.Text = "8 oz. Top Sirloin\r\n15.49";
+            this.grill4.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // grill3
             // 
-            this.button3.Location = new System.Drawing.Point(720, 360);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(233, 84);
-            this.button3.TabIndex = 39;
-            this.button3.Text = "Riblet Basket\r\n$10.49";
-            this.button3.UseVisualStyleBackColor = true;
+            this.grill3.Location = new System.Drawing.Point(720, 360);
+            this.grill3.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.grill3.Name = "grill3";
+            this.grill3.Size = new System.Drawing.Size(233, 84);
+            this.grill3.TabIndex = 39;
+            this.grill3.Text = "Riblet Basket\r\n10.49";
+            this.grill3.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // grill2
             // 
-            this.button2.Location = new System.Drawing.Point(446, 360);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(233, 84);
-            this.button2.TabIndex = 38;
-            this.button2.Text = "Classic Cheesburger\r\n$9.49";
-            this.button2.UseVisualStyleBackColor = true;
+            this.grill2.Location = new System.Drawing.Point(446, 360);
+            this.grill2.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.grill2.Name = "grill2";
+            this.grill2.Size = new System.Drawing.Size(233, 84);
+            this.grill2.TabIndex = 38;
+            this.grill2.Text = "Classic Cheesburger\r\n9.49";
+            this.grill2.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // grill1
             // 
-            this.button1.Location = new System.Drawing.Point(193, 360);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(233, 84);
-            this.button1.TabIndex = 37;
-            this.button1.Text = "Whisky Bacon Burger\r\n$11.29";
-            this.button1.UseVisualStyleBackColor = true;
+            this.grill1.Location = new System.Drawing.Point(193, 360);
+            this.grill1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.grill1.Name = "grill1";
+            this.grill1.Size = new System.Drawing.Size(233, 84);
+            this.grill1.TabIndex = 37;
+            this.grill1.Text = "Whisky Bacon Burger\r\n11.29";
+            this.grill1.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
@@ -254,21 +254,21 @@
             this.ClientSize = new System.Drawing.Size(1584, 861);
             this.Controls.Add(this.button13);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.button11);
-            this.Controls.Add(this.button12);
+            this.Controls.Add(this.app4);
+            this.Controls.Add(this.app3);
+            this.Controls.Add(this.sand2);
+            this.Controls.Add(this.sand1);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
+            this.Controls.Add(this.app2);
+            this.Controls.Add(this.app1);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
+            this.Controls.Add(this.chick1);
+            this.Controls.Add(this.chick2);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.grill4);
+            this.Controls.Add(this.grill3);
+            this.Controls.Add(this.grill2);
+            this.Controls.Add(this.grill1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Apple";
@@ -279,21 +279,21 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button11;
-        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button app4;
+        private System.Windows.Forms.Button app3;
+        private System.Windows.Forms.Button sand2;
+        private System.Windows.Forms.Button sand1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button app2;
+        private System.Windows.Forms.Button app1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button chick1;
+        private System.Windows.Forms.Button chick2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button grill4;
+        private System.Windows.Forms.Button grill3;
+        private System.Windows.Forms.Button grill2;
+        private System.Windows.Forms.Button grill1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label6;
