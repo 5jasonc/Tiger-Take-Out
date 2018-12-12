aadfbd01849a9f578d99db244310509ec61a6b72
jxh9922 <jxh9922@rit.edu>
2018-12-12 16:16:58 -0500

    added global variables class

--- TigerTakeOut/TigerTakeOut/Variables.cs
+++ TigerTakeOut/TigerTakeOut/Variables.cs
@@ -8,8 +8,8 @@ namespace TigerTakeOut
 {
     class Variables
     {
-        //global list variables for the names and prices of items ordered
-       public static List<string> names = new List<string>();
+        //global lists that will hold the names and prices of each food item
+        public static List<string> names = new List<string>();
         public static List<double> prices = new List<double>();
     }
 }
