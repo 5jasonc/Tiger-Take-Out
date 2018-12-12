58d7e3eede816987625d4555db61cf9b2e8371e9
julshotal <juliahotaling@gmail.com>
2018-12-12 14:33:43 -0500

    added new function to all

--- TigerTakeOut/TigerTakeOut/Apple.cs
+++ TigerTakeOut/TigerTakeOut/Apple.cs
@@ -24,233 +24,65 @@ namespace TigerTakeOut
             Checkout checkout = new Checkout(names, prices);
             checkout.ShowDialog();
         }
+
         private void grill1_Click(object sender, EventArgs e)
         {
-            string foodItem = this.Text;
-            foodItem.Split('\n');
-
-            if (foodItem.Length == 2)
-            {
-                string foodName = char.ToString(foodItem[0]);
-                double foodPrice = Convert.ToDouble(foodItem[1]);
-
-                names.Add(foodName);
-                prices.Add(foodPrice);
-            }
-            if (names.Count > 5)
-            {
-                DisableButton();
-            }
+            GetItems(sender);
         }
 
         private void grill2_Click(object sender, EventArgs e)
         {
-            string foodItem2 = this.Text;
-            foodItem2.Split('\n');
-
-            if (foodItem2.Length == 2)
-            {
-                string foodName2 = char.ToString(foodItem2[0]);
-                double foodPrice2 = Convert.ToDouble(foodItem2[1]);
-
-                names.Add(foodName2);
-                prices.Add(foodPrice2);
-            }
-            if (names.Count > 5)
-            {
-                DisableButton();
-            }
+            GetItems(sender);
         }
 
         private void grill3_Click(object sender, EventArgs e)
         {
-            string foodItem3 = this.Text;
-            foodItem3.Split('\n');
-
-            if (foodItem3.Length == 2)
-            {
-                string foodName3 = char.ToString(foodItem3[0]);
-                double foodPrice3 = Convert.ToDouble(foodItem3[1]);
-
-                names.Add(foodName3);
-                prices.Add(foodPrice3);
-            }
-            if (names.Count > 5)
-            {
-                DisableButton();
-            }
+            GetItems(sender);
         }
 
         private void grill4_Click(object sender, EventArgs e)
         {
-            string foodItem4 = this.Text;
-            foodItem4.Split('\n');
-
-            if (foodItem4.Length == 2)
-            {
-                string foodName4 = char.ToString(foodItem4[0]);
-                double foodPrice4 = Convert.ToDouble(foodItem4[1]);
-
-                names.Add(foodName4);
-                prices.Add(foodPrice4);
-            }
-            if (names.Count > 5)
-            {
-                DisableButton();
-            }
+            GetItems(sender);
         }
 
         private void chick1_Click(object sender, EventArgs e)
         {
-            string foodItem5 = this.Text;
-            foodItem5.Split('\n');
-
-            if (foodItem5.Length == 2)
-            {
-                string foodName5 = char.ToString(foodItem5[0]);
-                double foodPrice5 = Convert.ToDouble(foodItem5[1]);
-
-                names.Add(foodName5);
-                prices.Add(foodPrice5);
-            }
-            if (names.Count > 5)
-            {
-                DisableButton();
-            }
+            GetItems(sender);
         }
 
         private void chick2_Click(object sender, EventArgs e)
         {
-            string foodItem6 = this.Text;
-            foodItem6.Split('\n');
-
-            if (foodItem6.Length == 2)
-            {
-                string foodName6 = char.ToString(foodItem6[0]);
-                double foodPrice6 = Convert.ToDouble(foodItem6[1]);
-
-                names.Add(foodName6);
-                prices.Add(foodPrice6);
-            }
-            if (names.Count > 5)
-            {
-                DisableButton();
-            }
+            GetItems(sender);
         }
 
         private void sand1_Click(object sender, EventArgs e)
         {
-            string foodItem6 = this.Text;
-            foodItem6.Split('\n');
-
-            if (foodItem6.Length == 2)
-            {
-                string foodName6 = char.ToString(foodItem6[0]);
-                double foodPrice6 = Convert.ToDouble(foodItem6[1]);
-
-                names.Add(foodName6);
-                prices.Add(foodPrice6);
-            }
-            if (names.Count > 5)
-            {
-                DisableButton();
-            }
+            GetItems(sender);
         }
 
         private void sand2_Click(object sender, EventArgs e)
         {
-            string foodItem6 = this.Text;
-            foodItem6.Split('\n');
-
-            if (foodItem6.Length == 2)
-            {
-                string foodName6 = char.ToString(foodItem6[0]);
-                double foodPrice6 = Convert.ToDouble(foodItem6[1]);
-
-                names.Add(foodName6);
-                prices.Add(foodPrice6);
-            }
-            if(names.Count > 5)
-            {
-                DisableButton();
-            }
+            GetItems(sender);
         }
 
         private void app1_Click(object sender, EventArgs e)
         {
-            string foodItem6 = this.Text;
-            foodItem6.Split('\n');
-
-            if (foodItem6.Length == 2)
-            {
-                string foodName6 = char.ToString(foodItem6[0]);
-                double foodPrice6 = Convert.ToDouble(foodItem6[1]);
-
-                names.Add(foodName6);
-                prices.Add(foodPrice6);
-            }
-            if (names.Count > 5)
-            {
-                DisableButton();
-            }
+            GetItems(sender);
         }
 
         private void app2_Click(object sender, EventArgs e)
         {
-            string foodItem6 = this.Text;
-            foodItem6.Split('\n');
-
-            if (foodItem6.Length == 2)
-            {
-                string foodName6 = char.ToString(foodItem6[0]);
-                double foodPrice6 = Convert.ToDouble(foodItem6[1]);
-
-                names.Add(foodName6);
-                prices.Add(foodPrice6);
-            }
-            if (names.Count > 5)
-            {
-                DisableButton();
-            }
+            GetItems(sender);
         }
 
         private void app3_Click(object sender, EventArgs e)
         {
-            string foodItem7 = this.Text;
-            foodItem7.Split('\n');
-
-            if (foodItem7.Length == 2)
-            {
-                string foodName7 = char.ToString(foodItem7[0]);
-                double foodPrice7 = Convert.ToDouble(foodItem7[1]);
-
-                names.Add(foodName7);
-                prices.Add(foodPrice7);
-            }
-            if (names.Count > 5)
-            {
-                DisableButton();
-            }
+            GetItems(sender);
         }
 
         private void app4_Click(object sender, EventArgs e)
         {
-            string foodItem8 = this.Text;
-            foodItem8.Split('\n');
-
-            if (foodItem8.Length == 2)
-            {
-                string foodName9 = char.ToString(foodItem8[0]);
-                double foodPrice10 = Convert.ToDouble(foodItem8[1]);
-
-                    names.Add(foodName9);
-                    prices.Add(foodPrice10);
-           
-            }
-            if (names.Count > 5)
-            {
-                DisableButton();
-            }
+            GetItems(sender);
         }
 
         public void DisableButton()
@@ -268,6 +100,27 @@ namespace TigerTakeOut
             app3.Enabled = false;
             app4.Enabled = false;
             
+        }
+
+        public void GetItems(object sender)
+        {
+            string foodItem = ((Button)sender).Text;
+            foodItem.Split('\n');
+
+            if (foodItem.Length == 2)
+            {
+                string foodName10 = char.ToString(foodItem[0]);
+                double foodPrice10 = Convert.ToDouble(foodItem[1]);
+
+                names.Add(foodName10);
+                prices.Add(foodPrice10);
+
+            }
+
+            if (names.Count > 5 && prices.Count > 5)
+            {
+                DisableButton();
+            }
         }
 
     }
--- TigerTakeOut/TigerTakeOut/Mandarin.cs
+++ TigerTakeOut/TigerTakeOut/Mandarin.cs
@@ -12,9 +12,43 @@ namespace TigerTakeOut
 {
     public partial class Mandarin : Form
     {
+        List<string> names = new List<string>();
+        List<double> prices = new List<double>();
+
         public Mandarin()
         {
             InitializeComponent();
+        }
+
+        private void chef1_Click(object sender, EventArgs e)
+        {
+            GetItems(sender);
+        }
+
+        public void DisableButton()
+        {
+            chef1.Enabled = false;
+        }
+
+        public void GetItems(object sender)
+        {
+            string foodItem = ((Button)sender).Text;
+            foodItem.Split('\n');
+
+            if (foodItem.Length == 2)
+            {
+                string foodName10 = char.ToString(foodItem[0]);
+                double foodPrice10 = Convert.ToDouble(foodItem[1]);
+
+                names.Add(foodName10);
+                prices.Add(foodPrice10);
+
+            }
+
+            if (names.Count > 5 && prices.Count > 5)
+            {
+                DisableButton();
+            }
         }
     }
 }
