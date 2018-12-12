961528c3ef161207162a7bd2b15b147f1a610cd4
jxh9922 <jxh9922@rit.edu>
2018-12-12 16:20:34 -0500

    commented out Application.Run for merge

--- TigerTakeOut/TigerTakeOut/Program.cs
+++ TigerTakeOut/TigerTakeOut/Program.cs
@@ -16,7 +16,8 @@ namespace TigerTakeOut
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Selection());
+            
+            //Application.Run(new Selection());
         }
     }
 }
